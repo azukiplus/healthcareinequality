cc6d2f81210f05c6d48fb73992bfa670a1ef1057
azuki <azuki@cs.washington.edu>
2022-09-14 03:05:06 -0700

    font sizes and other minor adjustments

--- index.js
+++ index.js
@@ -10,27 +10,10 @@
    * Initial functions
    */
   function init() {
-    // document.getElementById("intro-btn").addEventListener("click", createRipple);
     document.getElementById("intro-btn").addEventListener("click", showOpening);
     document.getElementById("choose-story-button").addEventListener("click", chooseYourStory);
     document.getElementById("hm-path").addEventListener("click", hispanicMaleStory);
   }
-
-  // function createRipple(event) {
-  //   const button = event.currentTarget;
-  //   const circle = document.createElement("span");
-  //   const diameter = Math.max(button.clientWidth, button.clientHeight);
-  //   const radius = diameter / 2;
-  //   circle.style.width = circle.style.height = `${diameter}px`;
-  //   circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-  //   circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-  //   circle.classList.add("ripple");
-  //   const ripple = button.getElementsByClassName("ripple")[0];
-  //   if (ripple) {
-  //     ripple.remove();
-  //   }
-  //   button.appendChild(circle);
-  // }
 
   function showOpening() {
     document.getElementById("opening").classList.remove("hidden");
